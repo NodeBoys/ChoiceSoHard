fc4cb8a6ff6fb79e4ffdfabf34755ebd29caacd4
Jackie Yeh <jackie@tellustek.com>
2014-11-26 18:24:54 +0800

    加入Getdata
    取得臺北觀光景點json，轉成Geojson輸出

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 var test = require('./test');
+var getdata = require('./getdata');
 
 router.route('/')
 	.get(test.first);
+
+router.route('/gettaipei')
+	.get(getdata.taipei);
 
 module.exports = router;
